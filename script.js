e22c11696ae8acee4a64ff117a4e70945705d7ec
spitzer4 <spitzer4@illinois.edu>
2025-04-14 13:58:01 -0500

    Font Awesome bug

--- script.js
+++ script.js
@@ -47,17 +47,3 @@ const displayList = () => {
 
 btnHamburger.addEventListener('click', displayList)
 
-const scrollUp = () => {
-	const btnScrollTop = document.querySelector('.scroll-top')
-
-	if (
-		body.scrollTop > 500 ||
-		document.documentElement.scrollTop > 500
-	) {
-		btnScrollTop.style.display = 'block'
-	} else {
-		btnScrollTop.style.display = 'none'
-	}
-}
-
-document.addEventListener('scroll', scrollUp)
